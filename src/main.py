8a0930af42d90c6d405ffa81855e71b6a771e06e
Italo Castilho <italocosilva@gmail.com>
2022-10-22 11:27:11 -0300

    Join all components to work together

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import mlflow
 import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from omegaconf.listconfig import ListConfig
 
 
 # This automatically reads in the configuration
@@ -20,7 +21,7 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
+        assert isinstance(config["main"]["execute_steps"], ListConfig)
         steps_to_execute = config["main"]["execute_steps"]
 
     # Download step
@@ -39,18 +40,51 @@ def go(config: DictConfig):
 
     if "preprocess" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the preprocess step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, 'preprocess'),
+            'main',
+            parameters={
+                'input_artifact': os.path.join(
+                    config['main']['project_name'],
+                    'raw_data.parquet:latest'
+                ),
+                'artifact_name': 'preprocessed_data.csv',
+                'artifact_description': 'Data after preprocessing'
+            }
+        )
 
     if "check_data" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the check_data step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, 'check_data'),
+            'main',
+            parameters={
+                'reference_artifact': config['data']['reference_dataset'],
+                'sample_artifact': os.path.join(
+                    config['main']['project_name'],
+                    'preprocessed_data.csv:latest'
+                ),
+                'ks_alpha': config['data']['ks_alpha']
+            }
+        )
 
     if "segregate" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the segregate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, 'segregate'),
+            'main',
+            parameters={
+                'input_artifact': os.path.join(
+                    config['main']['project_name'],
+                    'preprocessed_data.csv:latest'
+                ),
+                'artifact_root': 'data',
+                'artifact_type': 'split_data',
+                'test_size': config['data']['test_size'],
+                'random_state': config['main']['random_seed'],
+                'stratify': config['data']['stratify'],
+            }
+        )
 
     if "random_forest" in steps_to_execute:
 
@@ -60,13 +94,38 @@ def go(config: DictConfig):
         with open(model_config, "w+") as fp:
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
-        ## YOUR CODE HERE: call the random_forest step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, 'random_forest'),
+            'main',
+            parameters={
+                'train_data': os.path.join(
+                    config['main']['project_name'],
+                    'data_train.csv:latest'
+                ),
+                'model_config': model_config,
+                'export_artifact': config['random_forest_pipeline']['export_artifact'],
+                'random_seed': config['main']['random_seed'],
+                'val_size': config['data']['val_size'],
+                'stratify': config['data']['stratify'],
+            }
+        )
 
     if "evaluate" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, 'evaluate'),
+            'main',
+            parameters={
+                'model_export': os.path.join(
+                    config['main']['project_name'],
+                    config['random_forest_pipeline']['export_artifact']+':latest'
+                ),
+                'test_data': os.path.join(
+                    config['main']['project_name'],
+                    'data_test.csv:latest'
+                ),
+            }
+        )
 
 
 if __name__ == "__main__":
